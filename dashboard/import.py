6d4eaa43cc9423a91aaf70e2e2d91b5a97366bef
Jan Kaluza <jkaluza@redhat.com>
2024-06-05 14:26:02 +0200

    Filter panels without any data
    Signed-off-by: Jan Kaluza <jkaluza@redhat.com>

--- dashboard/import.py
+++ dashboard/import.py
@@ -5,12 +5,68 @@ import requests
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
 import time
+from influxdb_client import InfluxDBClient
+dbuser = "grafana"
+
+if os.path.exists("/root/domecek/web/db.db"):
+    dbuser_code = "iNkFgdqP71ssfOJuTSEw2ZwiFbCIKCji1tNSa09-gWNZWUNDNypXN_fLnLjOVwK_zGoZRh6AwMTZpiGI7SMPyg=="
+else:
+    dbuser_code = "lJhdvtVrmDCLZ63fZgHJ3tzzD1wdK4QxrDQfAxmFoDFIyVbgbUXHN6ojt6q1A2Pc_14U2dKk9gPCqCKHg1xJSw=="
+client = InfluxDBClient(url="http://localhost:8086", token=dbuser_code, org="grafana")
+
 
 with open("../api_config.json") as f:
     cfg = json.loads(f.read())
     grafana_username = cfg["grafana_username"]
     grafana_password = cfg["grafana_password"]
     grafana_auth = HTTPBasicAuth(grafana_username, grafana_password)
+
+no_data_panel = {
+    "datasource": {
+    "type": "influxdb",
+    "uid": "fdn5d77g8x1xcf"
+    },
+    "gridPos": {
+    "h": 7,
+    "w": 24,
+    "x": 0,
+    "y": 0
+    },
+    "id": 2,
+    "options": {
+    "code": {
+        "language": "plaintext",
+        "showLineNumbers": False,
+        "showMiniMap": False
+    },
+    "content": "Váš dům neposílá žádná data využitelná v této nástěnce.\n\n",
+    "mode": "markdown"
+    },
+    "pluginVersion": "10.4.1",
+    "title": "Data nejsou k dispozici.",
+    "type": "text"
+}
+
+def filter_panels(client, dashboard):
+    query_api = client.query_api()
+    new_panels = []
+    for panel in dashboard["panels"]:
+        if "targets" not in panel:
+            new_panels.append(panel)
+            continue
+        query = str(panel["targets"][0]["query"])
+        query = query.replace("v.timeRangeStart", '-7d')
+        query = query.replace("v.timeRangeStop", '-1s')
+        query = query.replace("v.windowPeriod", '1h')
+        result = query_api.query(query=query)
+        if result:
+            new_panels.append(panel)
+
+    if not new_panels:
+        new_panels.append(no_data_panel)
+
+    dashboard["panels"] = new_panels
+    return dashboard
 
 
 r = requests.get(f'http://localhost:3000/api/orgs', auth=grafana_auth)
@@ -70,6 +126,7 @@ for org in r.json():
                 dashboard = json.loads(d)
                 del dashboard["id"]
                 del dashboard["uid"]
+                dashboard = filter_panels(client, dashboard)
                 for panel in dashboard["panels"]:
                     panel["datasource"]["uid"] = datasource_uid
                     for target in panel.get("targets", []):
@@ -132,8 +189,6 @@ for org in r.json():
     for i in range(10):
         r = requests.get(f'http://localhost:3000/api/v1/provisioning/contact-points', auth=grafana_auth, headers=headers)
         name2uid = {}
-        pprint(r.content, stream=sys.stderr)
-        pprint(r.json(), stream=sys.stderr)
         try:
             for data in r.json():
                 name2uid[data["name"]] = data["uid"]
