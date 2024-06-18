e4bda0988a416e7385c35c1f181de2a1993780ee
Jan Kaluza <jkaluza@redhat.com>
2024-06-18 09:29:25 +0200

    Add instrument.js
    Signed-off-by: Jan Kaluza <jkaluza@redhat.com>

---
+++ server/instrument.js
@@ -0,0 +1,19 @@
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+// Ensure to call this before requiring any other modules!
+Sentry.init({
+  dsn: "https://1e187b10b124fe5be05f85642e2b9b78@o4507392194969600.ingest.de.sentry.io/4507451490893904",
+  integrations: [
+    // Add our Profiling integration
+    nodeProfilingIntegration(),
+  ],
+
+  // Add Tracing by setting tracesSampleRate
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set sampling rate for profiling
+  // This is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
